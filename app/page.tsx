eb210e1d9c00a52d9c6f1acf52dad439edbcafce
singlexyz <singlexyz@gmail.com>
2023-06-02 23:04:02 +0800

    init commit.

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,133 @@
-import Image from 'next/image'
+"use client"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import * as React from "react"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from "@/components/ui/card"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from "@/components/ui/select"
+
+import { animated, useSpring, config } from "@react-spring/web"
+import { forwardRef, useId, useImperativeHandle, useRef } from "react"
+import { createPortal } from "react-dom"
+
+const AnimatedCard = animated(Card)
+
+type MotionFilterProps = {
+  children: (filter: string) => React.ReactNode
+}
+
+const MotionFilter = forwardRef(function MotionFilter(
+  { children }: MotionFilterProps,
+  ref
+) {
+  const id = useId()
+  const filter = `url(#${id})`
+  const filterRef = useRef<SVGFEGaussianBlurElement>(null)
+  useImperativeHandle(ref, () => ({
+    update(x: number, y: number) {
+      if (filterRef.current) {
+        filterRef.current.setAttribute("stdDeviation", `${x},${y}`)
+      }
+    }
+  }))
+  return (
+    <>
+      {createPortal(
+        <svg className="w-0 h-0 absolute">
+          <defs>
+            <filter id={id}>
+              <feGaussianBlur
+                ref={filterRef}
+                in="SourceGraphic"
+                stdDeviation="0,0"
+              />
+            </filter>
+          </defs>
+        </svg>,
+        document.body,
+        id
+      )}
+      {children(filter)}
+    </>
+  )
+})
 
 export default function Home() {
+  const styles = useSpring({
+    from: {
+      opacity: 0,
+      x: "-100vh"
+    },
+    to: {
+      opacity: 1,
+      x: "0vh"
+    },
+    config: config.gentle,
+    onChange(_, { springs }) {
+      if (ref.current) {
+        ref.current.update(0, Math.abs(springs.x.velocity * 200))
+      }
+    },
+    onResolve() {
+      if (ref.current) {
+        ref.current.update(0, 0)
+      }
+    }
+  })
+  const ref = useRef<{ update: (x: number, y: number) => void }>(null)
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 overflow-hidden">
+      <MotionFilter ref={ref}>
+        {(filter) => (
+          <AnimatedCard style={{ ...styles, filter }} className="w-[360px]">
+            <CardHeader>
+              <CardTitle className="tracking-wider">创造世界</CardTitle>
+              <CardDescription>轻而易举地创作出你的最爱</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <form>
+                <div className="grid w-full items-center gap-4">
+                  <div className="flex flex-col space-y-1.5">
+                    <Label htmlFor="name">姓名</Label>
+                    <Input id="name" placeholder="你连你是谁都不知道？" />
+                  </div>
+                  <div className="flex flex-col space-y-1.5">
+                    <Label htmlFor="status">状态</Label>
+                    <Select>
+                      <SelectTrigger id="status">
+                        <SelectValue placeholder="职业" />
+                        <SelectContent position="popper">
+                          <SelectItem value="next">学生</SelectItem>
+                          <SelectItem value="sveltekit">教师</SelectItem>
+                          <SelectItem value="astro">员工</SelectItem>
+                          <SelectItem value="nuxt">管理</SelectItem>
+                        </SelectContent>
+                      </SelectTrigger>
+                    </Select>
+                  </div>
+                </div>
+              </form>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+              <Button variant="ghost">离开，我不配</Button>
+              <Button className="tracking-widest">登录</Button>
+            </CardFooter>
+          </AnimatedCard>
+        )}
+      </MotionFilter>
     </main>
   )
 }
